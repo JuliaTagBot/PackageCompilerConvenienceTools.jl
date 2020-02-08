a30bb9b9ce10070c189329833a4f27b5a03a0e79
Dilum Aluthge <dilum@aluthge.com>
2020-02-08 13:14:18 -0500

    Update dependencies.jl

--- src/dependencies.jl
+++ src/dependencies.jl
@@ -5,7 +5,7 @@ import Pkg
     for pkginfo in values(Pkg.dependencies())
         push!(result, pkginfo.name)
     end
-    append!(result, collect(values(Pkg.Types.stdlib())))
+    append!(result, collect(values(Pkg.Types.stdlibs())))
     unique!(result)
     return result
 end
@@ -15,7 +15,7 @@ end
     for pkginfo in values(Pkg.dependencies())
         push!(pkg_names_to_add, pkginfo.name)
     end
-    append!(pkg_names_to_add, collect(values(Pkg.Types.stdlib())))
+    append!(pkg_names_to_add, collect(values(Pkg.Types.stdlibs())))
     unique!(pkg_names_to_add)
     Pkg.add(pkg_names_to_add)
     return nothing
